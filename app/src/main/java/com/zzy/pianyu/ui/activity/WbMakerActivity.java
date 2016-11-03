c987b3cf93a9c08110730fb611669ae9516ff4f7
zhiyou007 <279286948@qq.com>
2016-11-03 19:22:26 +0800

    fix bug

--- app/src/main/java/com/zzy/pianyu/ui/activity/WbMakerActivity.java
+++ app/src/main/java/com/zzy/pianyu/ui/activity/WbMakerActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.zzy.framework.Tools.Tools;
 import com.zzy.framework.adater.recyleview.CommonMultiTypeAdapter;
 import com.zzy.framework.adater.recyleview.ViewHolder;
@@ -16,6 +19,10 @@ import com.zzy.framework.adater.recyleview.base.ItemViewDelegate;
 import com.zzy.framework.base.BaseActivity;
 import com.zzy.pianyu.R;
 import com.zzy.pianyu.ui.bean.MakerBean;
+import com.zzy.pianyu.ui.widgets.sweetsheet.entity.MenuEntity;
+import com.zzy.pianyu.ui.widgets.sweetsheet.sweetpick.BlurEffect;
+import com.zzy.pianyu.ui.widgets.sweetsheet.sweetpick.RecyclerViewDelegate;
+import com.zzy.pianyu.ui.widgets.sweetsheet.sweetpick.SweetSheet;
 
 import java.util.ArrayList;
 
@@ -38,6 +45,9 @@ public class WbMakerActivity extends BaseActivity {
 
     @Bind(R.id.fab_speed_dial)
     FabSpeedDial fabSpeedDial;
+
+    @Bind(R.id.rl_view)
+    RelativeLayout rl_view;
 
     private ArrayList<MakerBean> mDatas = new ArrayList<MakerBean>();
 
@@ -144,8 +154,17 @@ public class WbMakerActivity extends BaseActivity {
                     }
 
                     @Override
-                    public void convert(ViewHolder holder, MakerBean makerBean, int position) {
-                        Tools.ToastMsg(mContext,"2");
+                    public void convert(ViewHolder holder, MakerBean makerBean,final int position) {
+                        //图片长按编辑事件
+                        SimpleDraweeView iv_img = holder.getView(R.id.iv_img);
+                        iv_img.setOnLongClickListener(new View.OnLongClickListener() {
+                            @Override
+                            public boolean onLongClick(View view) {
+                                //长按弹出操作选项
+                                setUpActionView(position);
+                                return false;
+                            }
+                        });
                     }
                 });
 
@@ -210,5 +229,57 @@ public class WbMakerActivity extends BaseActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void setUpActionView(int index)
+    {
+        final ArrayList<MenuEntity> list = new ArrayList<>();
+        //添加假数据
+        MenuEntity menuEntity1 = new MenuEntity();
+        menuEntity1.iconId = R.drawable.heart0;
+        menuEntity1.title = "code";
+        MenuEntity menuEntity = new MenuEntity();
+        menuEntity.iconId = R.drawable.heart0;
+        menuEntity.title = "QQ";
+        list.add(menuEntity1);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        list.add(menuEntity);
+        // SweetSheet 控件,根据 rl 确认位置
+        final SweetSheet mSweetSheet = new SweetSheet(rl_view);
+
+        //设置数据源 (数据源支持设置 list 数组,也支持从菜单中获取)
+        mSweetSheet.setMenuList(list);
+        //根据设置不同的 Delegate 来显示不同的风格.
+        mSweetSheet.setDelegate(new RecyclerViewDelegate(true));
+        //根据设置不同Effect 来显示背景效果BlurEffect:模糊效果.DimEffect 变暗效果
+        mSweetSheet.setBackgroundEffect(new BlurEffect(8));
+        //设置点击事件
+        mSweetSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+                //即时改变当前项的颜色
+
+                ((RecyclerViewDelegate) mSweetSheet.getDelegate()).notifyDataSetChanged();
+
+                //根据返回值, true 会关闭 SweetSheet ,false 则不会.
+                Toast.makeText(mContext, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        //Tools.ToastMsg(mContext,"xxxxxxxxxxxxxx");
+
+        mSweetSheet.toggle();
     }
 }
